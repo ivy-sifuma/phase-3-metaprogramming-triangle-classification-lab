23979e3aa0d66230cf9fd653fea51b7520887836
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-11-09 11:44:00 +0300

    Completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,30 +1,29 @@
 class Triangle
-  # write code here
+ # write code here
+ attr_accessor :sides
 
-  attr_accessor :sides
+ @sides = []
 
-  @sides = []
+ def initialize (side1, side2, side3)
+   @sides = [side1, side2, side3]
+   @sides.sort!
+ end
 
-  def initialize (side1, side2, side3)
-    @sides = [side1, side2, side3]
-    @sides.sort!
-  end
+ def kind
+   if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+     raise TriangleError
+   elsif @sides.uniq.length == 1
+     :equilateral
+   elsif @sides.uniq.length == 2
+     :isosceles
+   else
+     :scalene
+   end
+ end
 
-  def kind
-    if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
-      raise TriangleError
-    elsif @sides.uniq.length == 1
-      :equilateral
-    elsif @sides.uniq.length == 2
-      :isosceles
-    else
-      :scalene
-    end
-  end
+ class TriangleError < StandardError
 
-  class TriangleError < StandardError
-
-  end
+ end
 end
 
-end
+  
